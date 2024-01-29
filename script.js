d6294a422cb2a0cf7f2b53e2c3608320b86ab2f1
evilimas <eg.vilimas@gmail.com>
2024-01-29 14:10:20 +0100

    fixed conversion function

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const conversionBtn = document.getElementById('conversion-btn');
 const inputEl = document.getElementById('input-el');
-let lengthEl = document.getElementById('length-el');
-let volumeEl = document.getElementById('volume-el');
-let massEL = document.getElementById('mass-el');
+const lengthEl = document.getElementById('length-el');
+const volumeEl = document.getElementById('volume-el');
+const massEL = document.getElementById('mass-el');
 
 const meterToFeet = 3.281;
 const literToGallon = 0.264;
 const kilogramToPound = 2.204;
 
 conversionBtn.addEventListener('click', function () {
-  let inputValue = inputEl.value;
+  const inputValue = inputEl.value;
 
   if (isNaN(inputValue)) {
     alert('Please enter a number!');
@@ -26,8 +26,8 @@ conversionBtn.addEventListener('click', function () {
 
   // Length
 
-  let metToFeet = inputValue * meterToFeet;
-  let feetToMet = inputValue / meterToFeet;
+  const metToFeet = inputValue * meterToFeet;
+  const feetToMet = inputValue / meterToFeet;
 
   lengthEl.textContent = `${inputValue} meter = ${metToFeet.toFixed(
     3
@@ -35,8 +35,8 @@ conversionBtn.addEventListener('click', function () {
 
   // Volume
 
-  let litToGallon = inputValue * literToGallon;
-  let gallonToLit = inputValue / literToGallon;
+  const litToGallon = inputValue * literToGallon;
+  const gallonToLit = inputValue / literToGallon;
 
   volumeEl.textContent = `${inputValue} liters = ${litToGallon.toFixed(
     3
@@ -44,27 +44,28 @@ conversionBtn.addEventListener('click', function () {
 
   //Mass
 
-  let kiloToPound = inputValue * kilogramToPound;
-  let PoundToKilo = inputValue / kilogramToPound;
+  const kiloToPound = inputValue * kilogramToPound;
+  const PoundToKilo = inputValue / kilogramToPound;
 
   massEL.textContent = `${inputValue} kilogram = ${kiloToPound.toFixed(
     3
   )} pounds | ${inputValue} pounds = ${PoundToKilo.toFixed(3)} kilograms`;
 });
 
-// Dark mode
+// Dark/Light mode toggle function
+
 document.getElementById('toggle').addEventListener('click', function () {
-  let bodyClass = document.querySelector('.body');
-  let sectionClass1 = document.querySelector('.section1');
-  let sectionClass2 = document.querySelector('.section2');
-  let sectionClass3 = document.querySelector('.section3');
-  let svgEl = document.querySelector('.svg-icon');
-  let h3El1 = document.querySelector('.h3-1');
-  let h3El2 = document.querySelector('.h3-2');
-  let h3El3 = document.querySelector('.h3-3');
-  let p1El = document.getElementById('length-el');
-  let p1E2 = document.getElementById('volume-el');
-  let p1E3 = document.getElementById('mass-el');
+  const bodyClass = document.querySelector('.body');
+  const sectionClass1 = document.querySelector('.section1');
+  const sectionClass2 = document.querySelector('.section2');
+  const sectionClass3 = document.querySelector('.section3');
+  const svgEl = document.querySelector('.svg-icon');
+  const h3El1 = document.querySelector('.h3-1');
+  const h3El2 = document.querySelector('.h3-2');
+  const h3El3 = document.querySelector('.h3-3');
+  const p1El = document.getElementById('length-el');
+  const p1E2 = document.getElementById('volume-el');
+  const p1E3 = document.getElementById('mass-el');
 
   p1El.classList.toggle('p-dark');
   p1E2.classList.toggle('p-dark');
